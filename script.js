34659a7c2847359f8dee254ec2483099bd6f50a1
MANISHKA SINGH <manishka110518@gmail.com>
2024-03-03 16:34:45 +0530

    Quote generator

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//Show loading
+ function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+ }
+
+ //Hide Laoding
+ function complete() {
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+ }
 
 let apiQuotes =[];
 
@@ -16,19 +31,41 @@ function newQuote() {
     } else {
     authorText.textContent = quote.author;
     }
+    //Check the quote length
+    if(quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+
+
     quoteText.textContent = quote.text;
 } 
 
 // Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
        const response = await fetch(apiUrl);
        apiQuotes = await response.json();
        newQuote();
+       //stop loader
+       complete();
     } catch (error) {
-
+        getQuotes();
     }
+    
+//Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 }
 // on load
 getQuotes();
